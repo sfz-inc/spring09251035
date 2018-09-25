b256c1bd7ead4f75d6dd14a40dbfb1e15c61f168
Ryan Day <jrday@users.noreply.github.com>
2018-09-25 10:35:25 -0400

    Initial commit from Atomist

---
+++ src/main/java/atm/Spring09251035Application.java
@@ -0,0 +1,12 @@
+package atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09251035Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09251035Application.class, args);
+	}
+}
